40eaed9e9866a8ed1f61d54efc400386af9b9ef7
Abel Koech <koechabel161@gmail.com>
2024-06-29 18:16:18 +0300

    speed detector

--- speed.js
+++ speed.js
@@ -1,1 +1,26 @@
 //Write your code here
+function speedCheck(speed){
+    const speedLimit= 70;
+    const kmPerDemerit= 5;
+    const maxDemerit=12;
+    if(speed<70){
+        return 'Ok';
+    }else {
+        const excessSpeed= (speed-speedLimit);
+        const demeritPoints=math.floor(excessSpeed / kmPerDemerit);
+        if(demeritPoints>maxDemerit){
+            return 'License suspended.';
+        }else{
+            return (`Demerit pionts:${demeritPoints}`);
+        }
+    }
+}
+function main(){
+    let speed =parseFloat(prompt('input car speed in(km/hr):'));
+    if(!isNaN(speed)){
+       let result=speedCheck(speed);
+       alert(result);
+    }else{
+        alert('Invalid input');
+    }
+}main();
